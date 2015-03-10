d4d93ff709f841c7a856977786ef9083301c6fde
Omar Abdelhafith <omar.abdelhafith@mttnow.com>
2015-03-10 13:50:52 +0000

    adding tests

---
+++ omar_tests.py
@@ -0,0 +1,14 @@
+import unittest
+from ChessBoard import ChessBoard
+
+class OmarTests(unittest.TestCase):
+
+    def setUp(self):
+        self.cb = ChessBoard()
+
+    def testThatFirstTurnIsWhite(self):
+        assert self.cb.getTurn() == self.cb.WHITE
+
+    def testThatAfterWhiteMoveTheTurnIsBlack(self):
+        self.cb.addTextMove('e2e4')
+        assert self.cb.getTurn() == self.cb.BLACK
