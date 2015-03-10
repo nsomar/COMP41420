af19c515be33c23a9a202b8532bfd66ecdd4ed25
Cagri Cubukcu <cagri.cubukcu@gmail.com>
2015-03-10 16:55:50 +0000

    Updated my tests in cagri_tests to be more PEP8 compliant. Added unit tests for the add_move method.

---
+++ add_move_tests.py
@@ -0,0 +1,66 @@
+import unittest
+from ChessBoard import ChessBoard
+
+
+class CagriTests(unittest.TestCase):
+
+    def setUp(self):
+        self.cb = ChessBoard()
+
+    def test_game_not_over(self):
+        assert not self.cb._game_result
+
+    def test_invalid_from_location(self):
+        assert not self.cb.add_move((-1, 0), (2, 4))
+
+    def test_invalid_to_location(self):
+        assert not self.cb.add_move((3, 5), (-1, 6))
+
+    def test_if_different_from_and_to(self):
+        assert not self.cb.add_move((6, 2), (6, 2))
+
+    # At the start of the game the board is at its initial state
+    # Hence, it returns true for the check at (4,2) and false on others
+    def test_if_target_free(self):
+        assert self.cb.isFree(4, 2)
+        assert not self.cb.isFree(1, 1)
+        assert not self.cb.isFree(7, 7)
+
+    # At the initial state of the game turn is on the WHITE player
+    # 7,7 belongs to the WHITE so it returns true while 1,1 belongs to
+    # BLACK and returns false
+    def test_if_piece_correct_color(self):
+        assert not (self.cb.getColor(1, 1) == self.cb._turn)
+        assert (self.cb.getColor(7, 7) == self.cb._turn)
+
+    # This should be false at the beginning as its an ending condition
+    def test_if_fifty_rule_matches(self):
+        assert not (self.cb._fifty == 100)
+
+    # This should be false at the beginning as its an ending condition
+    def test_three_repetitions(self):
+        assert not self.cb.threeRepetitions()
+
+    # Testing movement of a white pawn from c2 to c3
+    def test_move_pawn(self):
+        assert self.cb.add_move((2, 6), (2, 5))
+
+    # To test the rook movement, first the blocking pawn must be moved
+    # After moving the blocking pawn, a black pawn must be moved to be
+    # able to move the rook.
+    def test_move_rook(self):
+        self.cb.add_move((0, 6), (0, 5))
+        self.cb.add_move((6, 1), (6, 2))
+        assert self.cb.add_move((0, 7), (0, 6))
+
+    # To test the bishop movement, first the blocking pawn must be moved
+    # After moving the blocking pawn on the top left corner,
+    # a black pawn must be moved to be able to move the bishop.
+    def test_move_bishop(self):
+        self.cb.add_move((1, 6), (1, 5))
+        self.cb.add_move((6, 1), (6, 2))
+        assert self.cb.add_move((2, 7), (1, 6))
+
+    # def test_move_queen(self):
+    # def test_move_king(self):
+    # def test_move_knight(self):
--- cagri_tests.py
+++ cagri_tests.py
@@ -1,87 +1,94 @@
 import unittest
 from ChessBoard import ChessBoard
 
+
 class MyChessBoard(ChessBoard):
-    def add_move(self, fromPos, toPos):
-        self.called_from_pos = fromPos
-        self.called_to_pos = toPos
+
+    def add_move(self, from_pos, to_pos):
+        self.called_from_pos = from_pos
+        self.called_to_pos = to_pos
+
 
 class CagriTests(unittest.TestCase):
 
     def setUp(self):
         self.cb = MyChessBoard()
 
+    ######################################
+    # Unittests for add_text_move method #
+    ######################################
+
     # Check if the (0,0) and (0,7) is movable
-    def testThatA1A8Movable(self):
+    def test_a1_a8_movable(self):
         self.cb.add_text_move('a1a8')
-        assert self.cb.called_from_pos == (0,7)
-        assert self.cb.called_to_pos == (0,0)
+        assert self.cb.called_from_pos == (0, 7)
+        assert self.cb.called_to_pos == (0, 0)
 
     # Check if 'a0a9' is not movable (out of bounds)
-    def testThatA0A9NotMovable(self):
-        assert self.cb.add_text_move('a0a9') == False
+    def test_a0_a9_not_movable(self):
+        assert not self.cb.add_text_move('a0a9')
 
     # Check if 'a1a9' is not movable (out of bounds)
-    def testThatA1A9NotMovable(self):
-        assert self.cb.add_text_move('a1a9') == False
+    def test_a1_a9_not_movable(self):
+        assert not self.cb.add_text_move('a1a9')
 
     # Check if the (7,0) and (7,7) is movable
-    def testThatH1H8Movable(self):
+    def test_h1_h8_movable(self):
         self.cb.add_text_move('h1h8')
-        assert self.cb.called_from_pos == (7,7)
-        assert self.cb.called_to_pos == (7,0)
+        assert self.cb.called_from_pos == (7, 7)
+        assert self.cb.called_to_pos == (7, 0)
 
     # Check if 'h0h9' is not movable (out of bounds)
-    def testThatH1H9NotMovable(self):
-        assert self.cb.add_text_move('h0h9') == False
+    def test_h0_h9_not_movable(self):
+        assert not self.cb.add_text_move('h0h9')
 
     # Check if 'h1h9' is not movable (out of bounds)
-    def testThatH1H9NotMovable(self):
-        assert self.cb.add_text_move('h1h9') == False
+    def test_h1_h9_not_movable(self):
+        assert not self.cb.add_text_move('h1h9')
 
     # Checks if the caps lock commands are not working
-    def testThatCapsAreNotWorking(self):
-        assert self.cb.add_text_move('A3F5') == False
+    def test_caps_are_not_working(self):
+        assert not self.cb.add_text_move('A3F5')
 
     # Checks if many moves are working
     # Accepts the last 2 commands
-    def testThatManyMovesAreWorking(self):
+    def test_multiple_moves_working(self):
         self.cb.add_text_move('b7e3d2a5')
-        assert self.cb.called_from_pos == (3,6)
-        assert self.cb.called_to_pos == (0,3)
+        assert self.cb.called_from_pos == (3, 6)
+        assert self.cb.called_to_pos == (0, 3)
 
     # Tests below this line checks if the moves defined in the main are valid moves.
-    def testThatE2E4Movable(self):
+    def test_e2_e4_movable(self):
         self.cb.add_text_move('e2e4')
-        assert self.cb.called_from_pos == (4,6)
-        assert self.cb.called_to_pos == (4,4)
+        assert self.cb.called_from_pos == (4, 6)
+        assert self.cb.called_to_pos == (4, 4)
 
-    def testThatF7F5Movable(self):
+    def test_f7_f5_movable(self):
         self.cb.add_text_move('f7f5')
-        assert self.cb.called_from_pos == (5,1)
-        assert self.cb.called_to_pos == (5,3)
+        assert self.cb.called_from_pos == (5, 1)
+        assert self.cb.called_to_pos == (5, 3)
 
-    def testThatE4F5Movable(self):
+    def test_e4_f5_movable(self):
         self.cb.add_text_move('e4f5')
-        assert self.cb.called_from_pos == (4,4)
-        assert self.cb.called_to_pos == (5,3)
+        assert self.cb.called_from_pos == (4, 4)
+        assert self.cb.called_to_pos == (5, 3)
 
-    def testThatG8H6Movable(self):
+    def test_g8_h6_movable(self):
         self.cb.add_text_move('g8h6')
-        assert self.cb.called_from_pos == (6,0)
-        assert self.cb.called_to_pos == (7,2)
+        assert self.cb.called_from_pos == (6, 0)
+        assert self.cb.called_to_pos == (7, 2)
 
-    def testThatF1D3Movable(self):
+    def test_f1_d3_movable(self):
         self.cb.add_text_move('f1d3')
-        assert self.cb.called_from_pos == (5,7)
-        assert self.cb.called_to_pos == (3,5)
+        assert self.cb.called_from_pos == (5, 7)
+        assert self.cb.called_to_pos == (3, 5)
 
-    def testThatH6F5Movable(self):
+    def test_h6_f5_movable(self):
         self.cb.add_text_move('h6f5')
-        assert self.cb.called_from_pos == (7,2)
-        assert self.cb.called_to_pos == (5,3)
+        assert self.cb.called_from_pos == (7, 2)
+        assert self.cb.called_to_pos == (5, 3)
 
-    def testThatD3F5Movable(self):
+    def test_d3_f5_movable(self):
         self.cb.add_text_move('d3f5')
-        assert self.cb.called_from_pos == (3,5)
-        assert self.cb.called_to_pos == (5,3)
+        assert self.cb.called_from_pos == (3, 5)
+        assert self.cb.called_to_pos == (5, 3)
